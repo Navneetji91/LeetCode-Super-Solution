ea4200824082a1fc155f156ab901dd895ae89f79
Navneet <148572640+Navneetji91@users.noreply.github.com>
2026-09-08 00:15:17 +0530

    Time: 1 ms (100%), Space: 47.2 MB (63.86%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.java
@@ -1,29 +1,29 @@
 class Solution {
     public int[] sortedSquares(int[] nums) {
-        // int n=nums.length;
-        // int[] arr=new int[n];
-        // int i=0;
-        // int j=n-1;
-        // while(i<=j){
-        //     if(Math.abs(nums[i]) < Math.abs(nums[j])){
-        //         arr[n-1]=nums[j]*nums[j];
-        //         n--;
-        //         j--;
-        //     }else{
-        //         arr[n-1]=nums[i]*nums[i];
-        //         n--;
-        //         i++;
-        //     }
-        // }
-        // return arr;
-
         int n=nums.length;
-        for(int i=0;i<n;i++){
-            nums[i]=nums[i]*nums[i];
+        int[] arr=new int[n];
+        int i=0;
+        int j=n-1;
+        while(i<=j){
+            if(Math.abs(nums[i]) < Math.abs(nums[j])){
+                arr[n-1]=nums[j]*nums[j];
+                n--;
+                j--;
+            }else{
+                arr[n-1]=nums[i]*nums[i];
+                n--;
+                i++;
+            }
         }
-        Arrays.sort(nums);
-        System.out.println(Arrays.toString(nums));
-        return nums;
+        return arr;
+
+        // int n=nums.length;
+        // for(int i=0;i<n;i++){
+        //     nums[i]=nums[i]*nums[i];
+        // }
+        // Arrays.sort(nums);
+        // System.out.println(Arrays.toString(nums));
+        // return nums;
     }
 }
 
